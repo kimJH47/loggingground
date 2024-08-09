69dab1bd2a45185b7b9e0dc0a4b759e2d783cd03
kimJaeHyeon <kmr2644@gmail.com>
2024-08-09 19:17:13 +0900

    fix: 어노테이션 오타수정

--- src/main/java/core/loggingground/presentation/config/WebConfig.java
+++ src/main/java/core/loggingground/presentation/config/WebConfig.java
@@ -1,17 +1,19 @@
 package core.loggingground.presentation.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import core.loggingground.presentation.interceptor.LoggingInterceptor;
 
-@Configurable
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggingInterceptor())
-			.addPathPatterns("/api/*");
+			.addPathPatterns("/api/**");
 	}
 }
